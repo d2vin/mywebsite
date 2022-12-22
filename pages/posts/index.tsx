b5e794bb57ad63a0e5bda25e27644ebe55545ab3
Devin Minnihan <devinminnihan@gmail.com>
2022-12-21 20:39:20 -0500

    even newer theme fix

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -16,26 +16,28 @@ export async function getStaticProps() {
 
 export default function Index({ allPostsData }: any) {
   return (
-    <Layout>
-      <section className="transition-all ml-4 mr-4 md:mx-0 sm:pt-20 md:pt-20 pt-20">
-        <h1 className="sm:text-6xl text-3xl transition-all">
-          🗒️ Featured Posts
-        </h1>
-        <ul className="pt-8">
-          {allPostsData.map(({ id, date, title }: any) => (
-            <>
-              <Link key={id} href={`posts/${id}`}>
-                <div className="cursor-pointer w-7/8 border-b border-black dark:border-gray-200 py-3 transform hover:scale-[1.01] transition-all">
-                  <div className="flex justify-between">
-                    <h1>{title}</h1>
-                    <div>{date}</div>
+    <div className="h-screen mb-20">
+      <Layout>
+        <section className="transition-all ml-4 mr-4 md:mx-0 sm:pt-20 md:pt-20 pt-20">
+          <h1 className="sm:text-6xl text-3xl transition-all">
+            🗒️ Featured Posts
+          </h1>
+          <ul className="pt-8">
+            {allPostsData.map(({ id, date, title }: any) => (
+              <>
+                <Link key={id} href={`posts/${id}`}>
+                  <div className="cursor-pointer w-7/8 border-b border-black dark:border-gray-200 py-3 transform hover:scale-[1.01] transition-all">
+                    <div className="flex justify-between">
+                      <h1>{title}</h1>
+                      <div>{date}</div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+                </Link>
+              </>
+            ))}
+          </ul>
+        </section>
+      </Layout>
+    </div>
   );
 }
