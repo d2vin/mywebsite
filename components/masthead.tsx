7462164ce383fd9eba7e349e685d92ba9b49002c
Devin Minnihan <devinminnihan@gmail.com>
2023-02-02 22:58:21 -0500

    bio fix

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -3,23 +3,23 @@ import Image from 'next/image';
 
 const Masthead = () => {
   return (
-    <div className="flex flex-col-reverse sm:flex-row items-start">
+    <div className="flex flex-col-reverse items-center sm:flex-row sm:items-start">
       <div className="max-w-lg flex flex-col pr-6 md:pl-0 sm:pt-20 md:pt-20">
-        <p className="sm:text-6xl text-3xl pl-4 md:pl-0 transition-all text-center sm:text-left">
+        <p className="sm:text-6xl text-3xl pl-4 md:pl-0 text-center sm:text-left">
           Devin Minnihan
         </p>
         <p className="text-2xl font-extralight pl-4 md:pl-0 transition-all text-center sm:text-left">
           Parsons 2022
         </p>
-        <p className="w-full pt-4 pl-4 md:pl-0 transition-all">
-          Software Developer studying{' '}
+        <p className="w-full text-center sm:text-left pt-4 pl-4 md:pl-0 transition-all">
+          Software Developer and graduate of the{' '}
           <a
             href="https://www.newschool.edu/parsons/bba-design-management/"
             rel="noreferrer"
             target="_blank"
             className="hover:text-red-500"
           >
-            Strategic Design & Management
+            Strategic Design & Management Program
           </a>{' '}
           at the{' '}
           <a
@@ -32,7 +32,7 @@ const Masthead = () => {
           </a>
         </p>
       </div>
-      <div className="w-[176px] sm:w-[176px] left-[50%] translate-x-[-50%] sm:left-0 sm:translate-x-0 relative mb-0 sm:mb-0 mr-auto pt-8 pl-4 pb-4 pr-4 md:pr-0">
+      <div className="w-[176px] sm:w-[176px] flex-shrink-0 left-[50%] translate-x-[-50%] sm:left-0 sm:translate-x-0 relative mb-0 sm:mb-0 mr-auto pt-8 pl-4 pb-4 pr-4 md:pr-0">
         <Image
           src="/me.jpg"
           width={267}
