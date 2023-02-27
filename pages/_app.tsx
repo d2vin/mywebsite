7387ba7cdd00d4c3a580bac835b47968b4d72ca0
Devin Minnihan <devinminnihan@gmail.com>
2023-02-27 14:38:09 -0500

    fix text color again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider, useTheme } from 'next-themes';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { theme, setTheme } = useTheme();
   useEffect(() => {
     const blob = document.getElementById('blob');
 
@@ -23,7 +24,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" enableSystem={true}>
       <div id="blob"></div>
-      <Component {...pageProps} />
+      <div className={`${theme === 'dark' ? 'text-white' : 'text-black'}`}>
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
