54e1d5711e95bf6abdf57c3bd6633c53f7cb018f
Devin Minnihan <devinminnihan@gmail.com>
2023-03-12 09:18:25 -0400

    add default theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   // }, []);
 
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme={'dark'}>
       {/* <div id="blob" className="collapse sm:visible"></div> */}
       <div className="dark:text-white text-black">
         <Component {...pageProps} />
