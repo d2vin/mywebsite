fbd95e227f28d9ac10159ce81b127871d6b8ef85
Devin Minnihan <devinminnihan@gmail.com>
2025-08-03 00:23:29 -0400

    fix: update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider, useTheme } from 'next-themes';
-import { useEffect } from 'react';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { theme, setTheme } = useTheme();
-  // useEffect(() => {
-  //   const blob = document.getElementById('blob');
-
-  //   document.body.onpointermove = (event) => {
-  //     const { clientX, clientY } = event;
-
-  //     blob?.animate(
-  //       {
-  //         left: `${clientX}px`,
-  //         top: `${clientY}px`,
-  //       },
-  //       { duration: 3000, fill: 'forwards' }
-  //     );
-  //   };
-  // }, []);
 
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme={'dark'}>
-      {/* <div id="blob" className="collapse sm:visible"></div> */}
       <div className="dark:text-white text-black">
         <Component {...pageProps} />
       </div>
