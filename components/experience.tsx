1582e9ce96ab43f1db20fb466cbd9a247f58c063
Devin Minnihan <devinminnihan@gmail.com>
2026-08-30 23:50:26 -0400

    fix: update

--- components/experience.tsx
+++ components/experience.tsx
@@ -8,6 +8,7 @@ const roles = [
   ['Maximal Asset Management', 'Contract Software Engineer', 'New York', '2024', 'Present', 'https://www.maximalam.com/'],
   ['Teleportium', 'Contract Software Engineer', 'New York', '2023', 'Present', 'https://www.teleportium.us/'],
   ['Anakin Clothing', 'Contract Software Engineer', 'New York', '2019', 'Present', 'https://anakin.clothing'],
+  ['Kaigan', 'Contract Software Engineer', 'New York', '2026', 'Present', 'https://kaigan.online/'],
   ['Bedlam', 'Contract Software Engineer', 'New York', '2025', 'Present', 'https://bedlamtokyo-c9b429e60a74fa729d53.o2.myshopify.dev/product-wall'],
   ['Digital Gallery', 'Freelance Software Engineer', 'New York', '2024', 'Present', 'https://kaito-mu.vercel.app/'],
   ['Baek Industries', 'Contract Software Engineer', 'New York', '2025', 'Present', 'https://baekindustries.com/'],
